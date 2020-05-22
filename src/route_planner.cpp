59ad8e9ab3e32ece680446a50f3c4476e9844aa5
thomasdunlap <tom.a.dunlap@gmail.com>
2020-05-22 16:35:39 -0700

    Currently compiles.

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,7 +10,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-
+    this->start_node = &m_Model.FindClosestNode(start_x, end_x);
+    this->end_node = &m_Model.FindClosestNode(start_y, end_y);
 }
 
 
@@ -19,9 +20,10 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - You can use the distance to the end_node for the h value.
 // - Node objects have a distance method to determine the distance to another node.
 
-float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-
+float RoutePlanner::CalculateHValue(RouteModel::Node const *node) { 
+    
 }
+
 
 
 // TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
@@ -32,7 +34,15 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 // - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
-
+    // expand current node add unvisted neighbors to open_list
+    current_node->FindNeighbors();
+    //for (auto* n : current_node.neighbors) {
+    //    n.parent = current_node
+    //    n.h_value = RoutePlanner::CalculateHValue(n);
+    //    n.g_value = ????
+    //    this->open_list.push_back(n);
+    //}
+    
 }
 
 
@@ -81,4 +91,6 @@ void RoutePlanner::AStarSearch() {
 
     // TODO: Implement your solution here.
 
+    RoutePlanner::AddNeighbors(current_node);
+    RoutePlanner::open_list.push_back(current_node);
 }
